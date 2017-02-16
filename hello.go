dbe0a3f8a4de912ac42a7e5bc3d2d003d2dc2698
nsilver7 <nicksilver@gmail.com>
2017-02-16 16:18:23 -0600

    modified:   hello.go

--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/nsilver7/stringutil"
+)
 
 func main() {
+	fmt.Println("nonce: ", time.Now().Unix()*10000)
 	fmt.Printf("hello, world\n")
+	fmt.Printf(stringutil.Reverse("hello, world")+"\n")
 }
